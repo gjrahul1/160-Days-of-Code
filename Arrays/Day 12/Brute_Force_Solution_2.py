be5c8bcf1fa4a351a65203c0009a2b31b2c63d17
G.J.Rahul <103945279+gjrahul1@users.noreply.github.com>
2025-10-17 22:31:25 +0530

    Create Brute_Force_Solution_2.py

---
+++ Arrays/Day 12/Brute_Force_Solution_2.py
@@ -0,0 +1,17 @@
+### 3 Sum
+
+def three_sum(nums):
+     nums.sort()
+     results = []
+    
+     for i in range(len(nums)):
+          for j in range(i+1, len(nums)):
+               for k in range(j+1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                         triplet = (nums[i], nums[j], nums[k])
+
+                         if triplet not in results:
+                                results.append([nums[i], nums[j], nums[k]])
+     return results
+                    
+print(three_sum([-1,0,1,2,-1,-4]))
